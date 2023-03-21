1c1bb68ba664d1a753a5296537e6245e4b486df0
Maria De Avelar Galvao Santos Lima <mde-avel@c2r8s4.42lisboa.com>
2023-03-21 21:43:53 +0000

    Rank02

---
+++ Rank02/level_1/ft_swap/ft_swap.c
@@ -0,0 +1,22 @@
+//#include<stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
+
+// int	main(void)
+// {
+// 	int	a;
+// 	int	b;
+
+// 	a = 10;
+// 	b = 20;
+// 	printf("a: |%d| & b: |%d|\n", a, b);
+// 	ft_swap(&a, &b);
+// 	printf("a: |%d| & b: |%d|\n", a, b);
+// }
