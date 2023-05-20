726193a7b68f2be08ed80b5893777fe3ad8ddecb
MariaStarLit <mde-avel@DESKTOP-R04921E.localdomain>
2023-05-21 00:36:33 +0100

    Rank02

--- Rank02/level_1/ft_swap/ft_swap.c
+++ Rank02/level_1/ft_swap/ft_swap.c
@@ -1,4 +1,4 @@
-//#include<stdio.h>
+#include<stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
@@ -9,14 +9,14 @@ void	ft_swap(int *a, int *b)
 	*b = aux;
 }
 
-// int	main(void)
-// {
-// 	int	a;
-// 	int	b;
+int	main(void)
+{
+int	a;
+int	b;
 
-// 	a = 10;
-// 	b = 20;
-// 	printf("a: |%d| & b: |%d|\n", a, b);
-// 	ft_swap(&a, &b);
-// 	printf("a: |%d| & b: |%d|\n", a, b);
-// }
+a = 10;
+b = 20;
+printf("a: |%d| & b: |%d|\n", a, b);
+ft_swap(&a, &b);
+printf("a: |%d| & b: |%d|\n", a, b);
+}
---
+++ Rank02/level_1/repeat_alpha/repeat_alpha.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void	ft_char(int i, char c)
+{
+	while (i >= 0)
+	{
+		write(1, &c, 1);
+		i--;
+	}
+}
+
+void	repeat_alpha(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			ft_char((str[i] - 'a'), str[i]);
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			ft_char((str[i] - 'A'), str[i]);
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		repeat_alpha(argv[1]);
+	}
+	write(1, "\n", 1);
+}
