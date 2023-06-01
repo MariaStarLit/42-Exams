665684c20a75e13faa986444336b0d2f66af0f45
MariaStarLit <mde-avel@DESKTOP-R04921E.localdomain>
2023-06-01 21:33:26 +0100

    Rank 02

--- Rank02/level_1/ft_swap/ft_swap.c
+++ Rank02/level_1/ft_swap/ft_swap.c
@@ -9,14 +9,14 @@ void	ft_swap(int *a, int *b)
 	*b = aux;
 }
 
-int	main(void)
-{
-int	a;
-int	b;
+// int	main(void)
+// {
+// int	a;
+// int	b;
 
-a = 10;
-b = 20;
-printf("a: |%d| & b: |%d|\n", a, b);
-ft_swap(&a, &b);
-printf("a: |%d| & b: |%d|\n", a, b);
-}
+// a = 10;
+// b = 20;
+// printf("a: |%d| & b: |%d|\n", a, b);
+// ft_swap(&a, &b);
+// printf("a: |%d| & b: |%d|\n", a, b);
+// }
--- Rank02/level_2/ft_atoi/ft_atoi.c
+++ Rank02/level_2/ft_atoi/ft_atoi.c
@@ -1,1 +1,33 @@
-//libft
+// #include <stdlib.h>
+// #include <stdio.h>
+
+int     ft_atoi(const char *str)
+{
+	int i = 0;
+	int sinal = 1;
+	int res = 0;
+
+	while(str[i] >= 0 && str[i] <= 32)
+		i++;
+	if (str[i] == '+')
+		i++;
+	if (str[i] == '-')
+	{
+		sinal = -1;
+		i++;
+	}
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		res = (str[i] - 48) + res * 10;
+		i++;
+	}
+	return (res * sinal);
+}
+
+// int	main(void)
+// {
+// 	const char	a[] = "  \r\t\n\f -2r24";
+
+// 	printf("mine: %d\n", ft_atoi(a));
+// 	printf("atoi: %d\n", atoi(a));
+// }
