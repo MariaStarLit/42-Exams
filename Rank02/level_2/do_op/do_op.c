a91559338bfb9abb39695594fac7734f4458be9c
MariaStarLit <mde-avel@DESKTOP-R04921E.localdomain>
2023-06-13 23:57:30 +0100

    Rank02

---
+++ Rank02/level_2/do_op/do_op.c
@@ -0,0 +1,26 @@
+#include "do_op.h"
+
+int	main(int ac, char **av)
+{
+	int res;
+	int d1;
+	int d2;
+	
+	if(ac == 4)
+	{
+		d1 = atoi(av[1]);
+		d2 = atoi(av[3]);
+		if(av[2][0] == '+')
+			res = d1 + d2;
+		if(av[2][0] == '-')
+			res = d1 - d2;
+		if(av[2][0] == '*')
+			res = d1 * d2;
+		if(av[2][0] == '/')
+			res = d1 / d2;
+		if(av[2][0] == '%')
+			res = d1 % d2;
+		printf("%d", res);
+	}
+	printf("\n");
+}
