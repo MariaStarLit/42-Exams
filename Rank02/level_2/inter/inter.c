bac41a2eb294a8ceec33ed5ab15884faa48dc1cc
Maria De Avelar Galvao Santos Lima <mde-avel@c1r20s3.42lisboa.com>
2023-06-14 20:36:00 +0100

    Rank02

--- Rank02/level_2/inter/inter.c
+++ Rank02/level_2/inter/inter.c
@@ -1,12 +1,32 @@
 #include <unistd.h>
 
+//Watch this video, it explains everything!
+//https://www.youtube.com/watch?v=NTyBvR8lJU8&t=3s
+
+void	print_chars(char *s1, char *s2)
+{
+	int tab[256] = {0};
+	int	i = 0;
+
+	while (s2[i])
+	{
+		tab[(int)s2[i]] = 1;
+		i++;
+	}
+	while (s1[i])
+	{
+		if(tab[(int)s1[i]] == 1)
+		{
+			write(1, &s1[i], 1);
+			tab[(int)s1[i]] = 2;
+		}
+		i++;
+	}
+}
+
 int	main(int ac, char **av)
 {
-	int i = 0;
-
 	if(ac == 3)
-	{
-
-	}
+		print_chars(av[1], av[2]);
 	write(1, "\n", 1);
 }
