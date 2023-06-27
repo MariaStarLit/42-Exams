997126c09c5bc34eadc8d8e1c3e798092354c6fb
Maria De Avelar Galvao Santos Lima <mde-avel@c2r9s4.42lisboa.com>
2023-06-27 18:17:37 +0100

    Rank02

--- Rank02/level_3/ft_atoi_base/ft_atoi_base.c
+++ Rank02/level_3/ft_atoi_base/ft_atoi_base.c
@@ -1,58 +1,76 @@
 #include <stdio.h>
 
-int	ft_atoi(const char *str, int base)
-{
-	int i = 0;
-	int	res = 0;
+// char	*low_case(const char *str)
+// {
+// 	int		i;
+// 	char	*s;
 
-	base += 48;
-	while(str[i] >= '0' && str[i] > base)
+// 	i = 0;
+// 	s = NULL;
+// 	while (s[i])
+// 	{
+// 		s[i] = str[i];
+// 		if (str[i] >= 'A' && str[i] <= 'F')
+// 			s[i] = str[i] + 32;
+// 		i++;
+// 	}
+// 	s[i] = '\0';
+// 	return (s);
+// }
+
+int	new_atoi(const char *s, int base)
+{
+	int	res;
+	int	i;
+
+	res = 0;
+	i = 0;
+	if (base >= 0 && base <= 10)
+		base += 48;
+	else if (base >= 11 && base <= 16)
+		base += 97 - 10;
+	while (s[i])
 	{
-		res = (str[i] - 48) + res * 10;
+		if (s[i] >= '0' && s[i] <= '9' && s[i] < base)
+		{
+			res = (s[i] - 48) + res * 10;
+			printf("number: |%d|\n", res);
+		}
+		else if (s[i] >= 'a' && s[i] <= 'f' && s[i] < base)
+		{
+			if (s[i] < 'd')
+				res = s[i] + res * 100;
+			else
+				res = s[i] + res * 1000;
+			printf("letter: |%d|\n", res);
+		}
+
 		i++;
 	}
 	return (res);
-}
-
-int	big_base(const char *str, int base)
-{
-	char	lowcase[] = "0123456789abcdef";
-	char	upcase[] = "0123456789ABCDEF";
-	int		i = 0;
-	int		res = 0;
-
-	while (str[i])
-	{
-		if ()
-		i++;
-	}
 }
 
 int	ft_atoi_base(const char *str, int str_base)
 {
 	int		res = 0;
 	int		sinal = 1;
+	// char	*s;
 
-	while (*str >= 0 && *str <= 32)
-		str++;
-	if(*str == '-' || *str == '+')
+	if (!str)
+		return (0);
+	if (*str == '-')
 	{
-		if (*str == '-')
-			sinal = -1;
+		sinal = -1;
 		str++;
 	}
-	if (str_base >= 0 && str_base <= 10)
-		res = ft_atoi(*str, str_base);
-	else if(str_base > 10 && str_base <= 16)
-	{
-		res = big_base(*str, str_base);
-	}
+	// s = low_case(str);
+	res = new_atoi(str, str_base);
 	return (res * sinal);
 }
 
-int	main(void)
-{
-	const char fio[] = "-236ad";
+// int	main(void)
+// {
+// 	const char	fio[] = "-45ad";
 
-	printf("atoi_base: |%d|\n", ft_atoi_base(fio, 7));
-}
+// 	printf("atoi_base: |%d|\n", ft_atoi_base(fio, 16));
+// }
