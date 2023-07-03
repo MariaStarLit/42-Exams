1f4f0022a3380e2a9f6cfb1310e9aea45f649548
MariaStarLit <116599565+MariaStarLit@users.noreply.github.com>
2023-07-03 19:10:43 +0100

    Rank02

--- Rank02/level_3/lcm/lcm.c
+++ Rank02/level_3/lcm/lcm.c
@@ -1,12 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
 
-unsigned int    lcm(unsigned int a, unsigned int b)
+unsigned int	lcm(unsigned int a, unsigned int b)
 {
-    b -= a;
-    return (b);
+	unsigned int	ret1;
+	unsigned int	ret2;
+
+	if (a == 0 || b == 0)
+		return (0);
+
+	if (a > b)
+	{
+		ret1 = a;
+		ret2 = b;
+	}
+	else
+	{
+		ret1 = b;
+		ret2 = a;
+	}
+	while (ret1 != ret2)
+	{
+		while (ret2 < ret1)
+			ret2 += b;
+		if (ret1 == ret2)
+			return (ret1);
+		ret1 += a;
+	}
+	return (ret1);
 }
 
-// int main(void)
+// int main()
 // {
-//     printf("%d\n", lcm(24, 42));
+//     int low_multi = lcm(42, 12);
+//     printf("%u\n", low_multi);
 // }
