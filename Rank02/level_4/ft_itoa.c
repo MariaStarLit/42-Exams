7e48c3fddc241abecfec7e58b40135b5a2f9b4a2
Maria De Avelar Galvao Santos Lima <mde-avel@c2r7s6.42lisboa.com>
2023-09-14 15:22:27 +0100

    Passed by Grademe

---
+++ Rank02/level_4/ft_itoa.c
@@ -0,0 +1,65 @@
+// Assignment name  : ft_itoa
+// Expected files   : ft_itoa.c
+// Allowed functions: malloc
+// --------------------------------------------------------------------------------
+
+// Write a function that takes an int and converts it to a null-terminated string.
+// The function returns the result in a char array that you must allocate.
+
+// Your function must be declared as follows:
+
+// char	*ft_itoa(int nbr);
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	find_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char *ft_itoa(int nbr)
+{
+	char	*res;
+	int		len;
+
+	if (nbr == -2147483648)
+		return ("-2147483648\0");
+	len = find_len(nbr);
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res) 
+		return (NULL);
+	res[len] = '\0';
+	if (nbr < 0) 
+	{
+		res[0] = '-';
+		nbr *= -1;
+	}
+	if (nbr == 0)
+	{
+		res[0] = '0';
+		return (res);
+	}
+	while (nbr) 
+	{
+		len--;
+		res[len] = nbr % 10 + 48;
+		nbr /= 10;
+	}
+	return (res);
+}
+
+// int	main(void)
+// {
+// 	printf("itoa: |%s|\n", ft_itoa(506));
+// }
