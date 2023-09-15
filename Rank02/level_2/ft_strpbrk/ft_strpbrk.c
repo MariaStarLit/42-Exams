4f68a99a209212146d3787e5b2d8a5722a1d9e4d
Maria De Avelar Galvao Santos Lima <mde-avel@c2r7s8.42lisboa.com>
2023-09-15 13:04:21 +0100

    Rank02

--- Rank02/level_2/ft_strpbrk/ft_strpbrk.c
+++ Rank02/level_2/ft_strpbrk/ft_strpbrk.c
@@ -4,29 +4,31 @@
 char	*ft_strpbrk(const char *s1, const char *s2)
 {
 	int	i;
+	int	j;
 
 	i = 0;
+	j = 0;
 	if (!s1 || !s2)
 		return (0);
-	while (*s1)
+	while (s1[j])
 	{
 		i = 0;
 		while (s2[i])
 		{
-			if (*s1 == s2[i])
+			if (s1[j] == s2[i])
 			{
-				return ((char *)s1);
+				return ((char *)&s1[j]);
 			}
 			i++;
 		}
-		s1++;
+		j++;
 	}
 	return (0);
 }
 
 // int	main(void)
 // {
-// 	char b[] = ".";
+// 	char b[] = " aP";
 // 	char a[] = "Sugar Pie Honney Bone!";
 
 // 	printf("minha_f: |%s|\n", ft_strpbrk(a, b));
