abddaeb987729d65c5953acfeacbc7dd97c4bc24
Maria De Avelar Galvao Santos Lima <mde-avel@c2r7s8.42lisboa.com>
2023-09-15 12:47:28 +0100

    Passed by Grademe

--- Rank02/level_1/search_and_replace.c
+++ Rank02/level_1/search_and_replace.c
@@ -14,6 +14,7 @@
 // If the second argument is not contained in the first one (the string)
 // then the program simply rewrites the string followed by a newline.
 
+
 // Examples:
 // $>./search_and_replace "Papache est un sabre" "a" "o"
 // Popoche est un sobre
