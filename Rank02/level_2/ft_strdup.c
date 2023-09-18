b89d2ab7f6831fc5bd014117521e2698311e0ac6
MariaStarLit <mde-avel@DESKTOP-R04921E.localdomain>
2023-09-18 21:04:53 +0100

    Passed by Grademe

---
+++ Rank02/level_2/ft_strdup.c
@@ -0,0 +1,44 @@
+// Assignment name  : ft_strdup
+// Expected files   : ft_strdup.c
+// Allowed functions: malloc
+// --------------------------------------------------------------------------------
+
+// Reproduce the behavior of the function strdup (man strdup).
+
+// Your function must be declared as follows:
+
+// char    *ft_strdup(char *src);
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char    *ft_strdup(char *src)
+{
+    int		i = 0;
+    int		len;
+    char	*res;
+
+    while (src[i])
+        i++;
+    len = i;
+    res = (char *)malloc(sizeof(char) * len);
+    if (!res)
+        return (NULL);
+    i = 0;
+    while (src[i])
+    {
+        res[i] = src[i];
+        i++;
+    }
+    res[i] = '\0';
+    return (res);
+}
+
+// int	main(void)
+// {
+// 	char	s[] = "Banana!";
+
+// 	printf("minhaf: |%s|\n", ft_strdup(s));
+// 	printf("strdup: |%s|\n", strdup(s));
+// }
---
+++ Rank02/level_4/fprime.c
@@ -0,0 +1,71 @@
+// Assignment name  : fprime
+// Expected files   : fprime.c
+// Allowed functions: printf, atoi
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a positive int and displays its prime factors on the
+// standard output, followed by a newline.
+
+// Factors must be displayed in ascending order and separated by '*', so that
+// the expression in the output gives the right result.
+
+// If the number of parameters is not 1, simply display a newline.
+
+// The input, when there is one, will be valid.
+
+// Examples:
+
+// $> ./fprime 225225 | cat -e
+// 3*3*5*5*7*11*13$
+// $> ./fprime 8333325 | cat -e
+// 3*3*5*5*7*11*13*37$
+// $> ./fprime 9539 | cat -e
+// 9539$
+// $> ./fprime 804577 | cat -e
+// 804577$
+// $> ./fprime 42 | cat -e
+// 2*3*7$
+// $> ./fprime 1 | cat -e
+// 1$
+// $> ./fprime | cat -e
+// $
+// $> ./fprime 42 21 | cat -e
+// $
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void fprime(int nbr)
+{
+    int n = 2;
+
+    if (nbr == 1)
+    {
+        printf("1");
+        return ;
+    } 
+    while (n <= nbr)
+    {
+        if (nbr % n == 0)
+        {
+            printf("%d", n);
+            if (n != nbr)
+                printf("*");
+            nbr /= n;
+            n--;
+        }
+        n++;
+    }
+}
+
+int main(int ac, char **av)
+{
+    int nbr;
+
+    if (ac == 2)
+    {
+        nbr = atoi(av[1]);
+        fprime(nbr);
+    }
+    printf("\n");
+}
