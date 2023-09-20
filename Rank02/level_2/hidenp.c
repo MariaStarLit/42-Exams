b4ecc67fa58f804bbeb3a9274e9dbfc047814092
MariaStarLit <mde-avel@DESKTOP-R04921E.localdomain>
2023-09-20 20:45:27 +0100

    Passesd by Grademe

--- Rank02/level_2/hidenp.c
+++ Rank02/level_2/hidenp.c
@@ -27,28 +27,23 @@
 
 #include <unistd.h>
 
-void	hiden(char *s1, char *s2)
+int	main(int ac, char **av)
 {
-	int	i;
-	int	j;
+	int	i = 0;
+	int	j = 0;
 
-	i = 0;
-	j = 0;
-	while (s1[i] && s2[j])
+	if (ac == 3)
 	{
-		if (s1[i] == s2[j])
+		while (av[2][i])
+		{
+			if (av[1][j] == av[2][i])
+				j++;
 			i++;
-		j++;
-	}
-	if (s1[i] == '\0')
+		}
+	if (av[1][j] == '\0')
 		write(1, "1", 1);
 	else
 		write(1, "0", 1);
-}
-
-int	main(int ac, char **av)
-{
-	if (ac == 3)
-		hiden(av[1], av[2]);
+	}
 	write(1, "\n", 1);
 }
