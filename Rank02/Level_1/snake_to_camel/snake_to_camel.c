f5546f643ca1e13c7972c1d0beac327a91d04503
Maria De Avelar Galvao Santos Lima <mde-avel@c2r5s1.42lisboa.com>
2023-09-22 15:42:28 +0100

    Rank02

--- Rank02/Level_1/snake_to_camel/snake_to_camel.c
+++ Rank02/Level_1/snake_to_camel/snake_to_camel.c
@@ -1,24 +1,46 @@
+// Assignment name  : snake_to_camel
+// Expected files   : snake_to_camel.c
+// Allowed functions: malloc, free, realloc, write
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a single string in snake_case format
+// and converts it into a string in lowerCamelCase format.
+
+// A snake_case string is a string where each word is in lower case, separated by
+// an underscore "_".
+
+// A lowerCamelCase string is a string where each word begins with a capital letter
+// except for the first one.
+
+// Examples:
+// $>./snake_to_camel "here_is_a_snake_case_word"
+// hereIsASnakeCaseWord
+// $>./snake_to_camel "hello_world" | cat -e
+// helloWorld$
+// $>./snake_to_camel | cat -e
+// $
+
 #include <stdlib.h>
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int	main(int ac, char **av)
 {
 	int	i;
 
 	i = 0;
-	if (argc == 2)
+	if (ac == 2)
 	{
-		while (argv[1][i])
+		while (av[1][i])
 		{
-			if (!((argv[1][i] >= 'a' && argv[1][i] <= 'z')
-					|| argv[1][i] == '_'))
+			if (!((av[1][i] >= 'a' && av[1][i] <= 'z')
+					|| av[1][i] == '_'))
 				break ;
-			if (argv[1][i] == '_')
+			if (av[1][i] == '_')
 			{
 				i++;
-				argv[1][i] = argv[1][i] - 32;
+				av[1][i] = av[1][i] - 32;
 			}
-			write (1, &argv[1][i], 1);
+			write (1, &av[1][i], 1);
 			i++;
 		}
 	}
