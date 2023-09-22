cd6fe5ce7f9915bdfa6eed998cbf758a5926676c
Maria De Avelar Galvao Santos Lima <mde-avel@c2r5s1.42lisboa.com>
2023-09-22 16:03:00 +0100

    Passed by Exam

--- Rank02/Level_2/hidenp.c
+++ Rank02/Level_2/hidenp.c
@@ -40,10 +40,10 @@ int	main(int ac, char **av)
 				j++;
 			i++;
 		}
-	if (av[1][j] == '\0')
-		write(1, "1", 1);
-	else
-		write(1, "0", 1);
+		if (av[1][j] == '\0')
+			write(1, "1", 1);
+		else
+			write(1, "0", 1);
 	}
 	write(1, "\n", 1);
 }
