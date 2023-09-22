6ea09cf6c676d3a28b6200aa12c789557de1f19e
Maria De Avelar Galvao Santos Lima <mde-avel@c2r5s1.42lisboa.com>
2023-09-22 16:14:36 +0100

    Passed by Exam

--- Rank02/Level_3/ft_list_foreach/ft_list_foreach.c
+++ Rank02/Level_3/ft_list_foreach/ft_list_foreach.c
@@ -24,12 +24,16 @@
 // }                 t_list;
 
 #include "ft_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void (*f)(void *);
 
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
-	t_list	*list_ptr;
+	t_list	*list_ptr = begin_list;
 
-	list_ptr = begin_list;
 	while (list_ptr)
 	{
 		(*f)(list_ptr->data);
