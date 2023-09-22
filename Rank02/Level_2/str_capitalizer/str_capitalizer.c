c932a75dd7dd7060c804b01adecea6d6060da122
Maria De Avelar Galvao Santos Lima <mde-avel@c2r5s1.42lisboa.com>
2023-09-22 16:56:55 +0100

    Passed by Exam

--- Rank02/Level_2/str_capitalizer/str_capitalizer.c
+++ Rank02/Level_2/str_capitalizer/str_capitalizer.c
@@ -28,44 +28,41 @@
 
 #include <unistd.h>
 
-int	main(int ac, char **av)
+char    *lower_case(char *s)
 {
-	int	i;
-	int	j;
-	int	flag;
+    int i = 0;
+    while(s[i])
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            s[i] += 32;
+        i++;
+    }
+    return (s);
+}
 
-	j = 1;
-	flag = 0;
-	if (ac >= 2)
-	{
-		while (av[j])
-		{
-			i = 0;
-			while (av[j][i])
-			{
-				if (av[j][i] >= 'A' && av[j][i] <= 'Z')
-					av[j][i] += 32;
-				i++;
-			}
-			i = 0;
-			while (av[j][i])
-			{
-				if (av[j][i] == ' ' || av[j][i] == '\t')
-					flag = 1;
-				if (av[j][i] >= 'a' && av[j][i] <= 'z' && i == 0)
-					av[j][i] -= 32;
-				if (av[j][i] >= 'a' && av[j][i] <= 'z' && flag == 1)
-				{
-					flag = 0;
-					av[j][i] -= 32;
-				}
-				write(1, &av[j][i], 1);
-				i++;
-			}
-			write(1, "\n", 1);
-			j++;
-		}
-	}
-	else
-		write(1, "\n", 1);
+int main(int ac, char **av)
+{
+    int i;
+    int j = 1;
+
+    if (ac > 1)
+    {
+        while (av[j])
+        {
+            i = 0;
+            av[j] = lower_case(av[j]);
+            while (av[j][i])
+            {
+                if (av[j][i] >= 'a' && av[j][i] <= 'z')
+                    if (av[j][i - 1] == ' ' || av[j][i - 1] == '\t' || av[j][i - 1] == '\0')
+                        av[j][i] -= 32;
+                write(1, &av[j][i], 1);
+                i++;
+            }
+            j++;
+            write(1, "\n", 1); 
+        }
+    }
+    else
+        write(1, "\n", 1);
 }
