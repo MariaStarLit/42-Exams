0dd043b8a139a2ac647c4f344c46d60c60d719a5
Maria De Avelar Galvao Santos Lima <mde-avel@c2r3s1.42lisboa.com>
2024-03-26 19:44:08 +0000

    Passed the Exam

--- Rank04/microshell.c
+++ Rank04/microshell.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 
@@ -23,72 +22,72 @@ void error_message(char *msg, char *plus)
 	write (2, "\n", 1);
 }
 
-int cd(char **av, int i)
+int ft_cd(char *path, int i)
 {
 	if (i != 2)
 	{
 		error_message("error: cd: bad arguments", NULL);
-		return (1); 
+		return (1);
 	}
-	else if (chdir(av[i]) == -1)
+	else if (chdir(path) == -1)
 	{
-		error_message("error: cd: cannot change directory to ", av[i]);
-		return (1); 
+		error_message("error: cd: cannot change directory to ", path);
+		return (1);
 	}
-	return (0); 
+	return (0);
 }
 
-int ft_execute(char **av, char **env, int i)
+int ft_execute(char **av, char **envp, int i)
 {
 	int fd[2];
-	int exit_stats;
-	int has_pipe = av[i] && !strcmp(av[i], "|");
+	int exit;
+	int haspipe = av[i] && !strcmp(av[i], "|");
 
-	if (has_pipe && pipe(fd) == -1)
+	if (haspipe && pipe(fd) == -1)
 	{
 		error_message("error: fatal", NULL);
 		return (1);
 	}
 	int pid = fork();
-	if (!pid)
+	if(!pid)
 	{
 		av[i] = 0;
-		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		if(haspipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 		{
 			error_message("error: fatal", NULL);
 			return (1);
 		}
-		execve(*av, av, env);
+		execve(*av, av, envp);
 		error_message("error: cannot execute ", *av);
 		return (1);
 	}
-	waitpid(pid, &exit_stats, 0);
-	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+	waitpid(pid, &exit, 0);
+	if(haspipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 	{
 		error_message("error: fatal", NULL);
 		return (1);
 	}
-	return WIFEXITED(exit_stats) && WEXITSTATUS(exit_stats); 
+	return WIFEXITED(exit) && WEXITSTATUS(exit);
 }
 
-int main (int ac, char **av, char **envp)
+int main(int ac, char **av, char **envp)
 {
-	int i = 0;
-	int exit_stats = 0;
+    int exit = 0;
+    int i = 0;
 
-	if (ac > 1)
-	{
-		while (av[i] && av[++i])
-		{
-			av += i;
-			i = 0;
-			while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
-				i++;
-			if (!strcmp(*av, "cd"))
-				exit_stats = cd (av, i);
-			else if (i)
-				exit_stats = ft_execute(av, envp, i);
-		}
-	}
-	return (exit_stats); 
+    if (ac > 1)
+    {
+        while (av[i] && av[++i])
+        {
+            av += i;
+            i = 0;
+            while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+                i++;
+            if (!strcmp(*av, "cd"))
+                exit = ft_cd(av[i - 1], i);
+            else if (i)
+                exit = ft_execute(av, envp, i);
+        }
+    }
+    return (exit);
 }
