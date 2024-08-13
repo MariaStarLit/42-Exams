7dc770016a7751a00ff5d6b06be11b90606f6062
MariaStarLit <mde-avel@DESKTOP-R04921E.localdomain>
2024-08-13 15:23:42 +0100

    Rank05

---
+++ Rank05/cpp_module00/Warlock.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+
+class Warlock
+{
+	private:
+		std::string name;
+		std::string title;
+		Warlock(void);
+		Warlock(const Warlock &copy);
+		Warlock &operator=(const Warlock &copy);
+
+	public:
+		Warlock(std::string n, std::string t);
+		~Warlock(void);
+		//Member functions
+		std::string const &getName() const;
+		std::string const &getTitle() const;
+		void	setTitle(const std::string &t);
+		void	introduce(void) const;
+};
