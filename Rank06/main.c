fde55d13302e5e657c526ff3e2513da655bbd902
Maria De Avelar Galvao Santos Lima <mde-avel@c1r5s8.42lisboa.com>
2025-07-03 20:17:15 +0100

    update to exam version of 2025

---
+++ Rank06/main.c
@@ -0,0 +1,100 @@
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+int extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int		i = 0;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+char *str_join(char *buf, char *add)
+{
+	char	*newbuf;
+	int		len;
+
+    if (buf == 0)
+		len = 0;
+    else
+        len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+    free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
+}
+
+
+int main(int ac, char **av)
+{
+    int     sockfd, connfd, len;
+    struct  sockaddr_in servaddr, cli; 
+
+    //socket create and verification
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+    {
+        printf("Socket creation failed ...\n");
+        exit(0);
+    }
+    else
+       printf("Socket sucessfully created ...\n"); 
+	bzero(&servaddr, sizeof(servaddr));
+
+    //assign IP, Port
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
+	servaddr.sin_port = htons(8081);
+
+    //Binding newly created socket to given IP and verifivation
+	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
+	{
+        printf("Socket bind failed ...\n");
+        exit(0);
+    }
+    else
+        printf("Socket sucessfully binded ...\n"); 
+	if (listen(sockfd, 10) != 0)
+	{
+        printf("Cannot listen.\n");
+        exit(0);
+    }
+
+    len = sizeof(cli);
+    connfd = accep(sockfd, (struct sockaddr *)&cli, &len);
+    if (connfd < 0)
+    {
+        printf("Server accept failed ...\n");
+        exit(0);
+    }
+    else
+       printf("Server accepted the client ...\n")
+	return (0);
+}
--- Rank06/mini_serv/mini_serv.c
+++ Rank06/mini_serv/mini_serv.c
@@ -1,15 +1,29 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <string.h>
-#include <strings.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <netinet/ip.h>
 
 typedef struct s_client
 {
 	int		id;
 	char	*msg;
 }t_client;
+
+int count = 0;
+int max_fd = 0;
+int id[65536];
+char *msg[65536];
+
+int maxfd = 0, sockfd = 0, next_id = 0;
+char    buf_read[1001];
+
+void fatal()
+{
+	write(2, "Fatal error\n", strlen("Fatal error\n"));
+	exit(1);
+}
 
 int main(int argc, char **argv)
 {
