b52938d8f42f3cc5ff8a2c8a8d43e1ce1eaf1305
MariaStarLit <mde-avel@DESKTOP-R04921E>
2025-07-12 19:01:59 +0100

    Start

--- Rank06/mini_serv/mini_serv.c
+++ Rank06/mini_serv/mini_serv.c
@@ -1,22 +1,11 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <netdb.h>
-#include <sys/socket.h>
+#include <sys/select.h>
 #include <netinet/ip.h>
-
-int count = 0;
-int max_fd = 0;
-int	sockfd;
-int ids[65536];
-int *msgs[65536];
-
-fd_set	rfds, wfds, afds;
-
-char buf_read[1001];
-//Not finised 
 
 int extract_message(char **buf, char **msg)
 {
@@ -47,26 +36,26 @@ int extract_message(char **buf, char **msg)
 char *str_join(char *buf, char *add)
 {
 	char	*newbuf;
-	size_t	len;
+	int		len;
 
-    if (buf == 0)
-		{len = 0;}
-    else
-		{len = strlen(buf);}
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
 	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
 	if (newbuf == 0)
 		return (0);
 	newbuf[0] = '\0';
-	if (buf)
+	if (buf != 0)
 	{
 		strcat(newbuf, buf);
-    	free(buf);
+		free(buf);
 	}
 	strcat(newbuf, add);
 	return (newbuf);
 }
 
-void fatal_error()
+void error_msg()
 {
 	write(2, "Fatal error\n", 12);
 	exit(1);
@@ -74,69 +63,53 @@ void fatal_error()
 
 int main(int ac, char **av)
 {
+	int		sockfd, connfd, len;
+	struct	sockaddr_in servaddr, cli;
+
 	if (ac != 2)
 	{
-		write(2, "Wrong number of arguments\n", strlen("Wrong number of arguments\n"));
-		return (1);
+		write(2, "Wrong number of arguments\n", 26);
+		exit(1);
 	}
 
-    //socket create and verification
-	FD_ZERO(&afds);
+	//socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1)
-		fatal_error();
-	max_fd = sockfd;
-	FD_SET(sockfd, &afds);
-
-	struct  sockaddr_in servaddr; 
+	{
+		printf("Socket creation failed ...\n");
+		exit(0);
+	}
+	else
+		printf("Socket sucessfully created ...\n"); 
 	bzero(&servaddr, sizeof(servaddr));
 
-    //assign IP, Port
+	//assign IP, Port
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
-	servaddr.sin_port = htons(atoi(av[1]));
+	servaddr.sin_port = htons(8081);
 
-    //Binding newly created socket to given IP and verifivation
+	//Binding newly created socket to given IP and verifivation
 	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
-		fatal_error();
-
-	if (listen(sockfd, SOMAXCONN) < 0)
-		fatal_error();
- 
-	while (1)
 	{
-		rfds = afds;
-		wfds = afds;
-		if (select(max_fd + 1, &rfds, &wfds, NULL, NULL) < 0)
-			continue;
-
-		for (int fd = 0; fd <= max_fd; fd++)
-		{
-			if (!FD_ISSET(fd, &rfds))
-			continue;
-
-			if (fd == sockfd)
-			{
-				socklen_t len = sizeof(servaddr);
-				int client_fd = accept(sockfd, (struct sockaddr *)&servaddr, &len);
-				if (client_fd < 0)
-					continue;
-				register_client(client_fd);
-				break;
-			}
-			else
-			{
-				
-			}
-		}
+		printf("Socket bind failed ...\n");
+		exit(0);
 	}
-	int connfd;
-    if (connfd < 0)
-    {
-        printf("Server accept failed ...\n");
-        exit(0);
-    }
-    else
-       printf("Server accepted the client ...\n");
-	return (0);
+	else
+		printf("Socket sucessfully binded ...\n"); 
+	if (listen(sockfd, 10) != 0)
+	{
+		printf("Cannot listen.\n");
+		exit(0);
+	}
+
+	len = sizeof(cli);
+	connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+	if (connfd < 0)
+	{
+		printf("Server accept failed ...\n");
+		exit(0);
+	}
+	else
+		printf("Server accepted the client ...\n");
 }
+
